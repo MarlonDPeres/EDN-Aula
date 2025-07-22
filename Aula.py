1b642f636dc01afef0b4d46d1029bcb583a49638
MarlonDPeres <marlondionizioperes@gmail.com>
2025-07-22 20:20:36 -0300

    Add files via upload

---
+++ Aula.py
@@ -0,0 +1,24 @@
+print("olá mundo!")
+
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma é:", soma)
+
+comprimento = 12  # em cm
+largura = 14      # em cm
+altura = 20       # em cm
+
+volume = comprimento * largura * altura
+print("O volume da caixa é:", volume, "cm³")
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
